4beba4cfafb7d53a20f8e33ee3a5af640c131c50
Michael Simmons <github@e7i.org>
2017-06-05 02:45:45 -0500

    refactor(Article): convert to dumb component

--- app/components/Article/index.js
+++ app/components/Article/index.js
@@ -5,34 +5,25 @@
  */
 
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
 import Img from 'components/Img';
-import { makeSelectArticles } from 'containers/ArticlesPage/selectors';
 
-export class Article extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    const item = this.props.item;
-    const preview = { __html: item.preview };
-    const content = { __html: item.content };
+const Article = ({ item }) => { // eslint-disable-line react/prefer-stateless-function
+  const preview = { __html: item.preview };
+  const content = { __html: item.content };
 
-    // Render the content into a list item
-    return (
-      <div>
-        <h1>{item.title}</h1>
-        <Img src={item.image_url} alt={item.title} />
-        <div dangerouslySetInnerHTML={preview}></div>
-        <div dangerouslySetInnerHTML={content}></div>
-      </div>
-    );
-  }
-}
-
-Article.propTypes = {
-  item: React.PropTypes.object,
+  return (
+    <div>
+      <h1>{item.title}</h1>
+      <Img src={item.image_url} alt={item.title} />
+      <div className="preview" dangerouslySetInnerHTML={preview}></div>
+      <div className="content" dangerouslySetInnerHTML={content}></div>
+    </div>
+  );
 };
 
-export default connect(createStructuredSelector({
-  articles: makeSelectArticles(),
-}))(Article);
+Article.propTypes = {
+  item: React.PropTypes.object.isRequired,
+};
+
+export default Article;
