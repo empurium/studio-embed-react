f19fa07826492cfdde004b7fc50ef66587886f1e
Michael Simmons <github@e7i.org>
2017-06-05 02:46:11 -0500

    test(Article): added some preliminary tests

--- app/components/Article/tests/index.test.js
+++ app/components/Article/tests/index.test.js
@@ -1,10 +1,39 @@
-// import { shallow, mount } from 'enzyme';
-// import React from 'react';
-// import { IntlProvider } from 'react-intl';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import LoadingIndicator from 'components/LoadingIndicator';
-// import Article from '../index';
+import Img from 'components/Img';
+import Article from '../index';
+
 
 describe('<Article />', () => {
-  it('should render without exploding');
+  let wrapper;
+  const article = {
+    title: 'Article 1',
+    image_url: 'thumbnail.png',
+    preview: 'Preview',
+    content: 'Content',
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Article item={article} />
+    );
+  });
+
+  it('should render', () => {
+    expect(wrapper.find(Article)).toBeTruthy();
+  });
+
+  it('should render the title', () => {
+    expect(wrapper.contains(<h1>{article.title}</h1>)).toBeTruthy();
+  });
+
+  it('should render the image', () => {
+    expect(wrapper.find(Img)).toBeTruthy();
+    expect(wrapper.find(Img).prop('src')).toEqual(article.image_url);
+  });
+
+  it('should render the preview with HTML');
+
+  it('should render the content with HTML');
 });
