7fd430eee7412cae424941b726c65d7e0108c72d
Michael Simmons <github@e7i.org>
2017-06-05 01:53:41 -0500

    test(ArticlesList): 90% coverage, must move ErrorComponent

--- app/components/ArticlesList/tests/index.test.js
+++ app/components/ArticlesList/tests/index.test.js
@@ -1,15 +1,50 @@
-// import { shallow, mount } from 'enzyme';
-// import React from 'react';
-// import { IntlProvider } from 'react-intl';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import List from 'components/List';
-// import LoadingIndicator from 'components/LoadingIndicator';
-// import ArticlesList from '../index';
+import List from 'components/List';
+import LoadingIndicator from 'components/LoadingIndicator';
+import Article from 'components/Article';
+import ArticleList from '../index';
 
 describe('<ArticlesList />', () => {
-  it('should render the loading indicator when its loading');
+  it('should render nothing if nothing given', () => {
+    const wrapper = shallow(
+      <ArticleList loading={false} error={false} articles={false} />
+    );
 
-  it('should render an error if loading failed');
+    expect(wrapper.isEmptyRender()).toBeTruthy();
+  });
 
-  it('should not render anything if nothing interesting is provided');
+  it('should render the loading indicator when its loading', () => {
+    const wrapper = shallow(
+      <ArticleList loading />
+    );
+
+    expect(wrapper.find(List)).toHaveLength(1);
+    expect(wrapper.find(List).prop('component')).toEqual(LoadingIndicator);
+  });
+
+  it('should render a errors', () => {
+    const wrapper = shallow(
+      <ArticleList error />
+    );
+
+    expect(wrapper.find(List)).toHaveLength(1);
+    expect(wrapper.find(List).prop('component').name).toEqual('ErrorComponent');
+  });
+
+  it('should render a list of articles with Article', () => {
+    const articles = [
+      { title: 'Article 1' },
+      { title: 'Article 2' },
+      { title: 'Article 3' },
+    ];
+    const wrapper = shallow(
+      <ArticleList error={false} articles={articles} />
+    );
+
+    expect(wrapper.find(List)).toHaveLength(1);
+    expect(wrapper.find(List).prop('component')).toEqual(Article);
+    expect(wrapper.find(List).prop('items')).toEqual(articles);
+  });
 });
