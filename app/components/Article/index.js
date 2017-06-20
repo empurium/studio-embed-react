506afe021ed3232e17714b3f8443e9f7461f4430
Michael Simmons <github@e7i.org>
2017-06-19 22:20:39 -0500

    refactor(article): use Content component for img max-width

--- app/components/Article/index.js
+++ app/components/Article/index.js
@@ -8,20 +8,26 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import Img from 'components/Img';
+import Content from './Content';
 
 const Article = ({ item }) => { // eslint-disable-line react/prefer-stateless-function
   const preview = { __html: item.preview };
   const content = { __html: item.content };
 
   return (
-    <div className="publication-studio-article">
+    <article className="publication-studio-article">
       <h1>
         <Link to={`/article/${item.slug_uri}`}>{item.title}</Link>
       </h1>
-      <Img src={item.image_url} alt={item.title} />
-      <div className="preview" dangerouslySetInnerHTML={preview} />
-      <div className="content" dangerouslySetInnerHTML={content} />
-    </div>
+
+      <Content>
+        <Img src={item.image_url} alt={item.title} />
+      </Content>
+
+      <Content className="article-preview" dangerouslySetInnerHTML={preview} />
+
+      <Content className="article-content" dangerouslySetInnerHTML={content} />
+    </article>
   );
 };
 
